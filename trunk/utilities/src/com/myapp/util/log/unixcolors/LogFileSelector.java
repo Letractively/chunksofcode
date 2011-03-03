25afc53cc61c117dfd334356e22741e38b796eaa
andre.ragg <andre.ragg@92fd951c-2788-16aa-bc7f-2090130f54c4>
2011-03-03 19:47:19 +0000

    logfileselector changed

--- trunk/utilities/src/com/myapp/util/log/unixcolors/LogFileSelector.java
+++ trunk/utilities/src/com/myapp/util/log/unixcolors/LogFileSelector.java
@@ -2,8 +2,8 @@ package com.myapp.util.log.unixcolors;
 
 import java.io.InputStream;
 
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
@@ -12,29 +12,25 @@ import ch.qos.logback.core.joran.spi.JoranException;
 public class LogFileSelector {
 
 
-    private static final Logger log = LoggerFactory
-            .getLogger(LogFileSelector.class);
+    private static final Logger log = LoggerFactory.getLogger(LogFileSelector.class);
 
 
 
-    public static void setLogConfig(String configFileName) {
+    public static void setLogConfig(String configFileName) throws Exception {
         LoggerContext ctx = (LoggerContext) LoggerFactory.getILoggerFactory();
         JoranConfigurator cfg = new JoranConfigurator();
         cfg.setContext(ctx);
         ctx.reset();
 
-        ClassLoader scl = ClassLoader.getSystemClassLoader();
-        InputStream in = scl.getResourceAsStream(configFileName);
+        ClassLoader cl = LogFileSelector.class.getClassLoader();
+        InputStream in = cl.getResourceAsStream(configFileName);
 
         try {
             cfg.doConfigure(in);
-            log.debug("----------------  log config changed to "
-                      + configFileName
-                      + ". ----------");
+            log.debug("----------------  log config changed to {}. ----------", configFileName);
 
         } catch (JoranException e) {
-            throw new RuntimeException("could not change log-config to "
-                                       + configFileName, e);
+            throw new Exception("could not change log-config to " + configFileName, e);
         }
     }
 }
