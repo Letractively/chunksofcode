c805446c98ba2e40eb8542d5fea072c586b901f8
andre.ragg <andre.ragg@92fd951c-2788-16aa-bc7f-2090130f54c4>
2012-03-04 02:35:04 +0000

    jiffybox installation notes update

--- trunk/misc-goodies/custom-ubuntu-installcd/jiffybox-installation-notes.sh
+++ trunk/misc-goodies/custom-ubuntu-installcd/jiffybox-installation-notes.sh
@@ -1,5 +1,6 @@
+
 ###############################################################################
-############### installation notes for lamp stack in ubuntu: ##################
+############### installation notes for jiffybox server: #######################
 ###############################################################################
 #
 # more info:
@@ -8,8 +9,9 @@
 # starting from a fresh ubuntu installation (profile: openssh-server)
 # you may start with the "custom installation cd" (see: create-automatic-ubuntu-cd.txt)
 #
-#
-# credentials used for example installations:#{{{
+# credentials used for test installations # {{{
+# if you want to setup a machine for testing purposes only (!), you may want to use
+# those for convenience: (they are also listed in the corresponding sections below)
 #-------------------------------------------------
 # login             username        password
 #-------------------------------------------------
@@ -18,33 +20,61 @@
 # mysql             user1           password1
 # joomla            bigboss         biggie2
 #-------------------------------------------------
-# (you will also find those credentials in the corresponding sections)
-#}}}
+# 
+# }}}
 
 
-# AT FIRST, INCREASE CONVENIENCE: {{{
+###############################################################################
+############### finalize ubuntu installation: #################################
 ###############################################################################
 #
-# enable ssh with automated login using public key:
 #
-# on CLIENT MACHINE, create private/public key pair (if not already present)
+# handy apps, nice to have:
+apt-get install vim zsh tree
+#
+# Enable secure automatic ssh login {{{:
+# On CLIENT MACHINE, create private/public key pair. (If not already present)
 ssh-keygen -t rsa -f ~/.ssh/andre # creates files: ~/.ssh/andre ~/.ssh/andre.pub
-#
-# if you do not have a ~/.ssh/config on your client,
-# create it with: echo 'Host *' > ~/.ssh/config
-echo 'IdentityFile ~/.ssh/andre' >> ~/.ssh/config
+# XXX if you do not have a ~/.ssh/config on your client yet, execute:
+{ echo 'Host *'; echo 'IdentityFile ~/.ssh/andre'; } > ~/.ssh/config
 #
 # Install public key to REMOTE MACHINE:
-cat ~/.ssh/andre.pub | ssh testuser@192.168.56.101 'cat >> ~/.ssh/authorized_keys2'
+cat ~/.ssh/andre.pub | ssh testuser@192.168.0.101 'cat >> ~/.ssh/authorized_keys2'
+# }}}
+
+# Customize zsh settings : {{{
+wget -O /etc/zsh/zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc # grml kicks ass!!!
+#
+# XXX add these lines to /etc/zsh/zshrc to bind some keys in zsh:
+########### start
+# bind keys ctrl+left and ctrl+right to jump wordwise like vim:
+# escape sequence for ctrl+left:   ^[[1;5D
+# escape sequence for ctrl+right:  ^[[1;5C
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
+bindkey "^H" backward-kill-word
+########### end
+#
+cp /etc/zsh/zshrc /root/.zshrc # apply for root
+# }}}
+
+# setup vim : {{{
+{ echo ':colorscheme desert'; echo ':syn on'; echo ':set foldmethod=marker' } >> /etc/vim/vimrc
 #
 # }}}
 
+
+###############################################################################
+############### setup the service software: ###################################
+###############################################################################
+#
+#
 # 1.) INSTALL THE LAMPP STACK {{{
 ###############################################################################
 #
 apt-get install tasksel
 tasksel install lamp-server
-# mysql root user password: 'semmel3'
+# mysql root user password: 'newpwd'
 #
 # }}}
 
@@ -89,17 +119,18 @@ rm /var/www/test.php
 #
 apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
 #
-# if we need to access our databases from other hosts, 
+# XXX if we need to access our databases from other hosts, 
 # comment 'bind-address = localhost' (or bind to ip, e.g.: '192.168.1.20')
 #
 # test mysql installation:
 mysql -u root -p # (exit prompt with '\q' )
 #
 # change root password
-mysqladmin -u root -p password s3cret # note that password may be stored in your shell history file
+mysqladmin -u root -p password s3cret # note: password may be stored in your shell history file
 #
 # create a database and a user:
 mysql -u root -p
+# on mysql prompt:
 # mysql> UPDATE mysql.user SET Password = PASSWORD('newpwd') WHERE User = 'root';
 # mysql> FLUSH PRIVILEGES;
 # mysql> CREATE DATABASE database1;
@@ -111,11 +142,13 @@ mysql -u root -p
 ###############################################################################
 #
 # config dir: /etc/phpmyadmin/ (config-db.cfg does not need to be edited)
+# (note: use the mysql-users to login)
 #
 apt-get install phpmyadmin
 #
 # include the phpmyadmin module in the apache configuration:
-# put the line 'Include /etc/phpmyadmin/apache.conf' into file '/etc/apache2/apache2.conf'
+# XXX append the line 'Include /etc/phpmyadmin/apache.conf' into file '/etc/apache2/apache2.conf'
+# (if not present)
 #
 # }}}
 
@@ -159,30 +192,10 @@ chmod og-rwx -R /var/www/
 #
 # }}}
 
-# 8.) SETUP SFTP SERVER (OPTIONAL) {{{
+# 8.) INSTALL TOMCAT AND JAVA {{{
 ###############################################################################
 #
-addgroup sftponly
-useradd -d /home/sftpuser -s /usr/lib/sftp-server -M -N -g sftponly sftpuser
-mkdir -p /home/sftpuser/uploads /home/sftpuser/.ssh
-chown sftpuser:sftponly /home/sftpuser/uploads /home/sftpuser/.ssh
-echo '/usr/lib/sftp-server' >> /etc/shells # register users shell
-#
-# enable automated ssh login: copy public key from testuser account:
-# (see section: "AT FIRST, INCREASE CONVENIENCE")
-cp ~testuser/.ssh/authorized_keys2 /home/sftpuser/.ssh/
-chown sftpuser:sftponly /home/sftpuser/.ssh/authorized_keys2
-chmod 600 /home/sftpuser/.ssh/authorized_keys2
-#
-# change the "Subsystem sftp" line in /etc/ssh/sshd_config to:
-Subsystem sftp internal-sftp
-# append these lines to /etc/ssh/sshd_config ()
-Match group sftponly
-    ChrootDirectory %h
-    X11Forwarding no
-    AllowTcpForwarding no
-    ForceCommand internal-sftp
-/etc/init.d/ssh restart #apply changes
+# TODO continue here
 #
 # }}}
 
