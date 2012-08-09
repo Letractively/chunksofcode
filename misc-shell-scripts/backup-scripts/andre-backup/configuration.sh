cb0ac6dbc6e8ab7245ad36d7cc0193c469dc6bd3
andre.ragg <andre.ragg@92fd951c-2788-16aa-bc7f-2090130f54c4>
2012-08-09 20:27:57 +0000

    reconfigure after file system rearrangement

--- misc-shell-scripts/backup-scripts/andre-backup/configuration.sh
+++ misc-shell-scripts/backup-scripts/andre-backup/configuration.sh
@@ -1,73 +1,78 @@
 #!/bin/bash
 ########################################################################
 
-	##################################
-	#
-	#	CONFIGURATION OF SYNC PROCESS:
-	#
-	#	customize backup process in this file.
-	#
-	##################################
+   ##################################
+   #
+   #   CONFIGURATION OF SYNC PROCESS:
+   #
+   #   customize backup process in this file.
+   #
+   ##################################
 
 HERE="$(cd "`dirname "$0"`"; pwd)"
 BACKUP_ROOT="$HERE/.."
 
-	#
-	#
-	# the target destination dir where all data will be stored:
-	# source folders will be backed up below this destination target directory:
-	#
-	# be careful: the trailing slash is important! (see rsync manual)
-	#
+   #
+   #
+   # the target destination dir where all data will be stored:
+   # source folders will be backed up below this destination target directory:
+   #
+   # be careful: the trailing slash is important! (see rsync manual)
+   #
 BACKUP_DESTINATION="${BACKUP_ROOT}/data/"
 
 
-	#
-	#
-	# the arguments that will be passed to the rsync command
-	#
-BACKUP_ARGS="--archive --delete --verbose"
+   #
+   #
+   # the arguments that will be passed to the rsync command
+   #
+#BACKUP_ARGS="--archive --delete --verbose"
+BACKUP_ARGS="-rlptD --delete --verbose"
 
 
-	#
-	# 0 or 1; if 0, the files will be copied to the backup destinantion:
-        # if 1, a dry-run will be performed to
-        # display what would have been done on syncing.
-        #
-        # a logfile will be created in both cases
-	#
+
+   #
+   # 0 or 1; if 0, the files will be copied to the backup destinantion:
+   # if 1, a dry-run will be performed to
+   # display what would have been done on syncing.
+   #
+   # a logfile will be created in both cases
+   #
 DRY_RUN=1
 
 
-	#
-	#
-	# a logfile where all activities will be logged:
-	# each run will be logged in a new file.
-	#
+   #
+   #
+   # a logfile where all activities will be logged:
+   # each run will be logged in a new file.
+   #
 LOGFILE="${BACKUP_ROOT}/logs/backup_$(date +%Y-%m-%d_%H-%M-%S).log"
 
 
-	#
-	# specify the dirs that should be synchronized with the backup
-	# 
-	# !!! WARNING: the names of the dirs must be unique. multiple, different
-	#              dirs with the same name will overwrite each other.
-	#
-	# !!! NOTE:    do NOT define dirs with a trailing slash, otherwise the
-	#              contents of the dir will be copied under the
-	#              backup root dir instead of the dir itself!
-	#
+   #
+   # specify the dirs that should be synchronized with the backup
+   # 
+   # !!! WARNING: the names of the dirs must be unique. multiple, different
+   #              dirs with the same name will overwrite each other.
+   #
+   # !!! NOTE:    do NOT define dirs with a trailing slash, otherwise the
+   #              contents of the dir will be copied under the
+   #              backup root dir instead of the dir itself!
+   #
 BACKUP_SOURCE_DIRS=(
-	"/media/datadisk/personal"
-        "/home/andre/workspace"
-	"/media/datadisk/verena"
-	"/media/datadisk/software"
-	"/media/datadisk/sound"
-	"/media/datadisk/books"
-	"/media/warez/games"
-	"/media/datadisk/vm"
-	"/media/datadisk/system-backup"
-	"/media/movies/doku"
-	"/media/movies/movies"
-	"/media/movies/music"
+    "/media/datadisk/personal"
+    "/home/andre/workspace"
+    "/media/datadisk/verena"
+    "/media/warez/virtualbox"
+    "/media/datadisk/books"
+    "/media/datadisk/sound"
+    "/media/datadisk/software"
+    "/media/warez/games"
+    "/media/movies/misc"
+    "/media/movies/doku"
+    "/media/movies/movies"
+    "/media/movies/series"
+    "/media/movies/music"
 )
+
+
