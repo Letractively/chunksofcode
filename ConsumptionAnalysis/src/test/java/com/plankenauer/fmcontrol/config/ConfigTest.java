1dc1732c788884ec7b96ec0396a1f9b7e1af7a13
andre.ragg <andre.ragg@92fd951c-2788-16aa-bc7f-2090130f54c4>
2012-10-10 20:11:34 +0000

    working example checked in

--- ConsumptionAnalysis/src/test/java/com/plankenauer/fmcontrol/config/ConfigTest.java
+++ ConsumptionAnalysis/src/test/java/com/plankenauer/fmcontrol/config/ConfigTest.java
@@ -5,27 +5,33 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.plankenauer.fmcontrol.jdbc.Connect;
 
 public class ConfigTest
 {
+    private static final String PROJECT01 = "project01";
     private static final String ABFRAGE01 = "ABFRAGE01.properties";
     private static final String ABFRAGE02 = "ABFRAGE02.properties";
     private static final String ABFRAGE03 = "ABFRAGE03.properties";
     private static final String ABFRAGE04 = "ABFRAGE04.properties";
     private static final String ABFRAGE05 = "ABFRAGE05.properties";
+    private static final String ABFRAGE20 = "ABFRAGE20.properties";
 
     private static final Logger log = Logger.getLogger(ConfigTest.class);
+    private static final String NL = System.getProperty("line.separator");
+
+    private static ConfigRepository repo;
 
 
-    @Test
-    public void test2() throws IOException, ConfigException {
+    @BeforeClass
+    public static void init() throws Exception {
         log.info("ENTERING");
         String userDir = System.getProperty("user.dir");
         log.trace("working dir: " + userDir);
@@ -34,11 +40,15 @@ public class ConfigTest
         File testclasses = new File(target, "test-classes");
         File testConfigRepository = new File(testclasses, "testConfigRepository");
 
-        ConfigRepository repo = new ConfigRepository(testConfigRepository);
+        repo = new ConfigRepository(testConfigRepository);
         log.debug("testConfigRepository:" + testConfigRepository);
 
+    }
+
+    @Test
+    public void test2() throws Exception {
         try {
-            repo.parseAllConfigs("project01");
+            repo.parseAllConfigs(PROJECT01);
             fail("error expected in " + ABFRAGE05);
 
         } catch (ConfigException expected) {
@@ -49,8 +59,8 @@ public class ConfigTest
                                + Constants.CK_CONNECTION_HOSTNAME + "'"));
         }
 
-        Map<String, Config> allConfigs = repo.parseAllValidConfigs("project01");
-        assertTrue(String.valueOf(allConfigs), allConfigs.size() == 4);
+        Map<String, Config> allConfigs = repo.parseAllValidConfigs(PROJECT01);
+        assertTrue(String.valueOf(allConfigs), allConfigs.size() == 5);
 
         for (Config c : allConfigs.values()) {
             assertFalse(String.valueOf(allConfigs),
@@ -60,39 +70,86 @@ public class ConfigTest
             assertFalse(String.valueOf(allConfigs), c.equalsIgnoreCase(ABFRAGE05));
         }
 
-        testAbfrage01(allConfigs);
-        testAbfrage02(allConfigs);
-        testAbfrage03(allConfigs);
-
-        try {
-            testAbfrage04(allConfigs);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         log.info("EXITING");
     }
 
-
-    private void testAbfrage01(Map<String, Config> allConfigs) {
-        Config c1 = allConfigs.get(ABFRAGE01);
+    @Test
+    public void testAbfrage01() throws Exception {
+        String abfrage = ABFRAGE01;
+        Config c1 = repo.parseConfig(PROJECT01, abfrage);
         assertTrue(c1 != null);
+
+        Connect connect = new Connect(c1);
+        if (connect.isConnectable()) {
+            List<String> columns = connect.getAllColumns();
+            log.info("columns of " + abfrage + ": " + dumpColumnSet(columns));
+        }
+
+//      GNUPlot plot = new GNUPlot(true);
     }
 
-    private void testAbfrage02(Map<String, Config> allConfigs) {
-        Config c2 = allConfigs.get(ABFRAGE02);
+    @Test
+    public void testAbfrage02() throws Exception {
+        String abfrage = ABFRAGE02;
+        Config c2 = repo.parseConfig(PROJECT01, abfrage);
         assertTrue(c2 != null);
+
+        Connect connect = new Connect(c2);
+        if (connect.isConnectable()) {
+            List<String> columns = connect.getAllColumns();
+            log.info("columns of " + abfrage + ": " + dumpColumnSet(columns));
+        }
     }
 
-    private void testAbfrage03(Map<String, Config> allConfigs) {
-        Config c3 = allConfigs.get(ABFRAGE03);
+    @Test
+    public void testAbfrage03() throws Exception {
+        String abfrage = ABFRAGE03;
+        Config c3 = repo.parseConfig(PROJECT01, abfrage);
         assertTrue(c3 != null);
+
+        Connect connect = new Connect(c3);
+        if (connect.isConnectable()) {
+            List<String> columns = connect.getAllColumns();
+            log.info("columns of " + abfrage + ": " + columns.size());
+        }
     }
 
-    private void testAbfrage04(Map<String, Config> allConfigs) throws Exception {
-        Config c4 = allConfigs.get(ABFRAGE04);
+    @Test
+    public void testAbfrage04() throws Exception {
+        String abfrage = ABFRAGE04;
+        Config c4 = repo.parseConfig(PROJECT01, abfrage);
         assertTrue(c4 != null);
+
         Connect connect = new Connect(c4);
-        connect.getAllColumns();
+
+        if (connect.isConnectable()) {
+            List<String> columns = connect.getAllColumns();
+            log.info("columns of " + abfrage + ": " + columns.size());
+        }
+    }
+
+    @Test
+    public void testAbfrage20() throws Exception {
+        String abfrage = ABFRAGE20;
+        Config cfg = repo.parseConfig(PROJECT01, abfrage);
+        assertTrue(cfg != null);
+
+        Connect connect = new Connect(cfg);
+        if (connect.isConnectable()) {
+            List<String> columns = connect.getAllColumns();
+            log.info("columns of " + abfrage + ": " + columns.size());
+        }
+    }
+
+    private static String dumpColumnSet(List<String> cols) {
+        StringBuilder bui = new StringBuilder();
+        bui.append("[");
+        for (String col : cols) {
+            bui.append(NL);
+            bui.append(col);
+        }
+        bui.append(NL);
+        bui.append("]");
+        return bui.toString();
     }
 }
